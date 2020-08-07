8546a84404e042040ebd8615aea2683e172eb463
nick migel <nickmigel2@gmail.com>
2020-08-07 12:08:20 -0400

    2 down 1 more to go

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,17 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
-
+    result = []
+    positive = {}
+    negative = {}
+    for i in range(len(a)):
+        if a[i] > 0:
+            positive[a[i]] = a[i]
+        elif a[i] < 0:
+            negative[a[i]] = a[i]
+    for key in positive:
+        if negative.get(key * -1):
+            result.append(positive[key])
     return result
 
 
