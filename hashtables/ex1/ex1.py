6a76d99c83b0e2d7e3936c8d6de5a8b53fcdc63c
nick migel <nickmigel2@gmail.com>
2020-08-07 11:55:14 -0400

    1 down 2 to go

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,15 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    dct = {}
+    reversed = {}
+    for i in range(length):
+        dct[i] = weights[i]
+        reversed[length - i-1] = weights[i]
+    return dct
 
-    return None
+
+array = [1, 12, 456, 4, 0, 6, 32, 467, 9]
+length = len(array)
+limit = 7
+print(get_indices_of_item_weights(array, length, limit))
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,25 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    dct = {}
+    route = []
+    for ticket in tickets:
+        dct[ticket.source] = ticket.destination
+
+    i = 0
+    start = "NONE"
+    while i < length:
+        route.append(dct[start])
+        start = dct[start]
+        i += 1
 
     return route
+
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+
+print(reconstruct_trip(tickets, len(tickets)))
